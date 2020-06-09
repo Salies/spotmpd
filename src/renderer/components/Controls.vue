6f61de4a453f5399483b3d22cd4a851877a5a064
Salies <serezane@protonmail.com>
2020-06-08 22:06:25 -0300

    Volume and verification system ready.

--- src/renderer/components/Controls.vue
+++ src/renderer/components/Controls.vue
@@ -1,12 +1,34 @@
 <template>
     <div class="player-controls">
         eae, sou o controls
+        <button @click="playback()">Play/Pause</button>
+        <input type="range" v-model="volume" min="0" max="100" step="1" @input="setvol">
     </div>
 </template>
 
 <script>
 export default{
-    name:'controls'
+    name:'controls',
+    data: function() {
+        return {
+            volume: null
+        };
+    },
+    methods:{
+        playback(){
+            this.$electron.ipcRenderer.send('playback');
+        },
+        setvol(e){
+            this.$electron.ipcRenderer.send('setvol', this.volume);
+        }
+    },
+    mounted(){
+        this.$electron.ipcRenderer.send('getDefaultData');
+
+        this.$electron.ipcRenderer.on('defaultDataRecieved', (event, data) => {
+            this.volume = data.volume
+        })
+    }
 }
 </script>
 
--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -1,11 +1,21 @@
 <template>
     <div class="wrapper">
         eae, sou o home
+        <button @click="load()">load songs</button>
     </div>
 </template>
 
 <script>
 export default{
-    name:'home'
+    name:'home',
+    methods: {
+        load(){
+            this.$electron.ipcRenderer.send('load-list', [
+                "Digital (Bandcamp)/Alestorm/Curse of the Crystal Coconut (Digital Deluxe)/Alestorm - Curse of the Crystal Coconut (Digital Deluxe) - 07 Pirate's Scorn.flac",
+                "Digital (Bandcamp)/Alestorm/Curse of the Crystal Coconut (Digital Deluxe)/Alestorm - Curse of the Crystal Coconut (Digital Deluxe) - 08 Shit Boat (No Fans).flac",
+                "Digital (Bandcamp)/Alestorm/Curse of the Crystal Coconut (Digital Deluxe)/Alestorm - Curse of the Crystal Coconut (Digital Deluxe) - 09 Pirate Metal Drinking Crew.flac",
+            ]);
+        }
+    }
 }
 </script>
